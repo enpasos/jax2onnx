54234b68d2eadfff6da166e8f23adefe8e193ed0
enpasos <matthias.unverzagt@enpasos.com>
2025-03-10 11:17:56 +0100

    jax numpy squeeze + transpose fix

--- jax2onnx/converter/plugins/plugin_registry_static.py
+++ jax2onnx/converter/plugins/plugin_registry_static.py
@@ -61,5 +61,6 @@ plugin_registry = {
     "jnp.matmul": "jax2onnx.converter.plugins.jax.numpy.matmul",
     "jnp.reshape": "jax2onnx.converter.plugins.jax.numpy.reshape",
     "jnp.squeeze": "jax2onnx.converter.plugins.jax.numpy.squeeze",
+    "jnp.transpose": "jax2onnx.converter.plugins.jax.numpy.transpose",
     "random_gamma": "jax2onnx.converter.plugins.jax.random.random_gamma",
 }
