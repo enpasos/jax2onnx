2477c208d200aa3a6b62e0cb3b41b4f4f21c7a27
enpasos <matthias.unverzagt@enpasos.com>
2025-03-27 16:08:26 +0100

    added tests

--- jax2onnx/plugins/examples/onnx_functions/onnx_functions_007.py
+++ jax2onnx/plugins/examples/onnx_functions/onnx_functions_007.py
@@ -1,26 +1,23 @@
-# file: jax2onnx/sandbox/onnx_functions_example.py
+# file: jax2onnx/plugins/examples/onnx_functions/onnx_functions_007.py
 
 
-from jax2onnx import to_onnx
 from flax import nnx
-import os
-import onnx
 import jax.numpy as jnp
 
-from jax2onnx.plugin_system import onnx_function
+from jax2onnx.plugin_system import onnx_function, register_example
 
 
 @onnx_function
-class MLPBlock(nnx.Module):
+class MLPBlock007(nnx.Module):
     """MLP block for Transformer layers."""
 
     def __init__(self, num_hiddens, mlp_dim, dropout_rate=0.1, *, rngs: nnx.Rngs):
         self.layers = [
             nnx.Linear(num_hiddens, mlp_dim, rngs=rngs),
-            # lambda x: nnx.gelu(x, approximate=False),
-            # nnx.Dropout(rate=0.1, rngs=rngs),
-            # nnx.Linear(mlp_dim, num_hiddens, rngs=rngs),
-            # nnx.Dropout(rate=0.1, rngs=rngs),
+            lambda x: nnx.gelu(x, approximate=False),
+            nnx.Dropout(rate=0.1, rngs=rngs),
+            nnx.Linear(mlp_dim, num_hiddens, rngs=rngs),
+            nnx.Dropout(rate=0.1, rngs=rngs),
         ]
 
     def __call__(self, x: jnp.ndarray, deterministic: bool = True) -> jnp.ndarray:
@@ -32,8 +29,8 @@ class MLPBlock(nnx.Module):
         return x
 
 
-@onnx_function
-class TransformerBlock(nnx.Module):
+# @onnx_function
+class TransformerBlock007(nnx.Module):
     """Transformer block with multi-head attention and MLP."""
 
     def __init__(
@@ -57,26 +54,35 @@ class TransformerBlock(nnx.Module):
             decode=False,
         )
         self.layer_norm2 = nnx.LayerNorm(num_hiddens, rngs=rngs)
-        self.mlp_block = MLPBlock(num_hiddens, mlp_dim, mlp_dropout_rate, rngs=rngs)
+        self.mlp_block = MLPBlock007(num_hiddens, mlp_dim, mlp_dropout_rate, rngs=rngs)
         self.dropout = nnx.Dropout(rate=attention_dropout_rate, rngs=rngs)
 
     def __call__(self, x: jnp.ndarray, deterministic: bool = True) -> jnp.ndarray:
         y = self.attention(self.layer_norm1(x))
-        y = x
         y = self.dropout(y, deterministic=deterministic)
         x = x + y
         return x + self.mlp_block(self.layer_norm2(x), deterministic)
 
 
-top_model = TransformerBlock(
-    num_hiddens=256,
-    num_heads=8,
-    mlp_dim=512,
-    attention_dropout_rate=0.1,
-    mlp_dropout_rate=0.1,
-    rngs=nnx.Rngs(0),
+register_example(
+    component="onnx_functions_007",
+    description="transformer block with nested mlp block",
+    # source="https:/",
+    since="v0.4.0",
+    context="examples.onnx_functions",
+    children=["MLPBlock007"],
+    testcases=[
+        {
+            "testcase": "007 transformer block",
+            "callable": TransformerBlock007(
+                num_hiddens=256,
+                num_heads=8,
+                mlp_dim=512,
+                attention_dropout_rate=0.1,
+                mlp_dropout_rate=0.1,
+                rngs=nnx.Rngs(0),
+            ),
+            "input_shapes": [("B", 10, 256)],
+        },
+    ],
 )
-onnx_model = to_onnx(top_model, [("B", 10, 256)])
-output_path = "./docs/onnx/sandbox/transformer_block.onnx"
-os.makedirs(os.path.dirname(output_path), exist_ok=True)
-onnx.save(onnx_model, output_path)
