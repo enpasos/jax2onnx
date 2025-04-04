cd19d4c959f46acc34a5a2709e1007740a86b78c
enpasos <matthias.unverzagt@enpasos.com>
2025-04-04 07:35:54 +0200

    completing shape info

--- jax2onnx/sandbox/testing.py
+++ jax2onnx/sandbox/testing.py
@@ -1,14 +1,32 @@
 import onnx
 
-model = onnx.load("docs/onnx/examples/onnx_functions/011_vit_conv_embedding.onnx")
+# model = onnx.load("docs/onnx/examples/onnx_functions/011_vit_conv_embedding.onnx")
+model = onnx.load("docs/onnx/examples/onnx_functions/006_one_function_outer.onnx")
 
-print("=== VALUE INFO BEFORE SHAPE INFERENCE ===")
-for vi in list(model.graph.value_info) + list(model.graph.output):
+
+def print_single_shape_info(vi):
     shape = [
         dim.dim_value if (dim.HasField("dim_value")) else "?"
         for dim in vi.type.tensor_type.shape.dim
     ]
     print(f"{vi.name}: {shape}")
 
-# Now run inference
-inferred = onnx.shape_inference.infer_shapes(model)
+
+def print_shape_info(model):
+    print("=== INPUT INFO ===")
+    for vi in model.graph.input:
+        print_single_shape_info(vi)
+
+    print("=== VALUE INFO ===")
+    for vi in model.graph.value_info:
+        print_single_shape_info(vi)
+
+    print("=== OUTPUT INFO ===")
+    for vi in model.graph.output:
+        print_single_shape_info(vi)
+
+
+print_shape_info(model)
+print("######### INFERENCE #########")
+model = onnx.shape_inference.infer_shapes(model)
+print_shape_info(model)
