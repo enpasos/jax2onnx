073c4e5d468998762c74618dcb2ddd1f4652a058
enpasos <matthias.unverzagt@enpasos.com>
2025-07-09 22:05:35 +0200

    bug fixes

--- jax2onnx/plugins/jax/numpy/reshape.py
+++ jax2onnx/plugins/jax/numpy/reshape.py
@@ -25,9 +25,7 @@ jnp.reshape_p = Primitive("jnp.reshape")
 jnp.reshape_p.multiple_results = False
 
 
-@register_primitive(
-    primitive_obj=jnp.reshape_p,
-    binding_factory=lambda: jnp.reshape,
+@register_primitive(  
     jaxpr_primitive=jnp.reshape_p.name,
     jax_doc="https://jax.readthedocs.io/en/latest/_autosummary/jax.numpy.reshape.html",
     onnx=[
