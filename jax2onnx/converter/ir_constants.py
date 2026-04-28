8b3f71940e5e9c6fd948d8d57f652528da2f19d7
enpasos <matthias.unverzagt@enpasos.com>
2026-04-28 07:56:06 +0200

    Fix constant folding for multi-output primitives

--- jax2onnx/converter/ir_constants.py
+++ jax2onnx/converter/ir_constants.py
@@ -9,7 +9,7 @@ from numpy.typing import NDArray
 class ConstantFolder:
     def __init__(self) -> None:
         self._known: Dict[int, NDArray[np.generic]] = {}
-        self._producer: Dict[int, Any] = {}
+        self._producer: Dict[int, tuple[Any, int]] = {}
         self._handlers: Dict[str, Callable[..., Any]] = {}
 
     def register_const(self, var: Any, value: np.ndarray) -> None:
@@ -19,8 +19,8 @@ class ConstantFolder:
     def install_producers(self, jaxpr: Any) -> None:
         self._producer.clear()
         for eqn in jaxpr.eqns:
-            for out in eqn.outvars:
-                self._producer[id(out)] = eqn
+            for output_index, out in enumerate(eqn.outvars):
+                self._producer[id(out)] = (eqn, output_index)
 
     def register_handler(
         self, primitive_name: str, handler: Callable[..., Any]
@@ -39,9 +39,10 @@ class ConstantFolder:
             self._known[vid] = cast_arr
             return cast_arr
 
-        eqn = self._producer.get(vid)
-        if eqn is None:
+        producer = self._producer.get(vid)
+        if producer is None:
             return None
+        eqn, output_index = producer
 
         handler = self._handlers.get(eqn.primitive.name)
         if handler is None:
@@ -59,7 +60,17 @@ class ConstantFolder:
         except Exception:
             return None
 
-        arr = np.asarray(out)
-        cast_arr = cast(NDArray[np.generic], arr)
-        self._known[vid] = cast_arr
-        return cast_arr
+        output_count = len(eqn.outvars)
+        if output_count == 1:
+            outputs = (out,)
+        elif isinstance(out, (tuple, list)):
+            outputs = tuple(out)
+            if len(outputs) != output_count:
+                return None
+        else:
+            return None
+
+        for outvar, raw_output in zip(eqn.outvars, outputs):
+            arr = np.asarray(raw_output)
+            self._known[id(outvar)] = cast(NDArray[np.generic], arr)
+        return self._known.get(id(eqn.outvars[output_index]))
