d34c62215b9a559ab441a37172b95a1737814dd7
enpasos <matthias.unverzagt@enpasos.com>
2026-06-18 12:58:43 +0200

    Migrate JAX image plugins to JAX compat imports

--- jax2onnx/plugins/jax/image/_common.py
+++ jax2onnx/plugins/jax/image/_common.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import Any, Callable, cast
 
-from jax.extend.core import Primitive
+from jax2onnx.plugins.jax._jax_compat import Primitive
 
 
 BoundCallable = Callable[..., Any]
